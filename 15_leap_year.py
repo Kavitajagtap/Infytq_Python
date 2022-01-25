fe1376da16417eca5f1d8d6d6025cf23b420dc25
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2022-01-25 16:59:31 +0530

    Create 15_leap_year.py

---
+++ 15_leap_year.py
@@ -0,0 +1,30 @@
+'''
+Write a Python program to generate the next 15 leap years starting from a given year. Populate the leap years into a list and display the list. 
+
+'''
+
+# Code ->
+
+#lex_auth_012693797166096384149
+
+def find_leap_years(given_year):
+    list_of_leap_years=[]
+    # Write your logic her
+    count=0
+    for i in range(1,64):
+        if(given_year%400==0):
+            list_of_leap_years.append(given_year)
+            count+=1
+        elif(given_year%100==0):
+            j=0           
+        elif(given_year%4==0):
+             list_of_leap_years.append(given_year)
+             count+=1
+        else:
+            j=0
+        if(count==15):
+            break
+        given_year=given_year+1
+    return list_of_leap_years
+list_of_leap_years=find_leap_years( 1996)
+print(list_of_leap_years)
