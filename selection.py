daf80d79fb3aa0f434880ead1df627fc554842e0
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2022-01-25 16:41:59 +0530

    Create selection.py

---
+++ selection.py
@@ -0,0 +1,32 @@
+'''
+Write a python program to find and display the product of three positive integer values based on the rule mentioned below:
+
+It should display the product of the three values except when one of the integer value is 7. In that case, 7 should not be included in the product and the values to its left also should not be included.
+If there is only one value to be considered, display that value itself. If no values can be included in the product, display -1.
+
+Note: Assume that if 7 is one of the positive integer values, then it will occur only once. Refer the sample I/O given below.
+
+Sample Input          1, 5, 3       3, 7, 8       7, 4, 3      1, 5, 7
+
+Expected Output         15             8             12            -1
+
+'''
+
+# code ->
+
+def find_product(num1,num2,num3):
+    product=0
+    if(num1 != 7 and num2 != 7 and num3 != 7):
+        product = num1*num2*num3
+        return product
+    elif(num1 == 7):
+        product = num2 * num3
+        return product
+    elif(num2 == 7):
+        return num3
+    else:
+        return -1
+
+# Provide different values for num1, num2, num3 and test your program
+product=find_product(1, 5, 7)
+print(product)
