24ed240faae848fdae1ca6dc71f4d53fc58f0226
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2022-02-15 07:40:46 +0530

    Update and rename Assignment on function arguments (mutable & immutable).py to Assignment_on_function_arguments_(mutable & immutable).py

--- Assignment_5/Assignment_on_function_arguments_(mutable & immutable).py
+++ Assignment_5/Assignment_on_function_arguments_(mutable & immutable).py
@@ -1,5 +1,6 @@
 '''
-Write a python function, find_pairs_of_numbers() which accepts a list of positive integers with no repetitions and returns count of pairs of numbers in the list that adds up to n.
+Write a python function, find_pairs_of_numbers() which accepts a list of positive integers with no repetitions and returns count of pairs of numbers in the list that adds up 
+to n.
 The function should return 0, if no such pairs are found in the list.
 
 Sample Input  :  1.  [1, 2, 7, 4, 5, 6, 0, 3], 6            2. [3, 4, 1, 8, 5, 9, 0, 6], 9
